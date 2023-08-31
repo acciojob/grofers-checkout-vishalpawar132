cf7dc0c8807221fdb15b786325f0f991be5564a0
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-08-31 09:15:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	 const priceElements = document.querySelectorAll(".price");
+
+  let total = 0;
+
+  // Calculate the total price
+  priceElements.forEach((priceElement) => {
+    total += parseFloat(priceElement.textContent);
+  });
+
+  // Create a new row for total price
+  const totalRow = document.createElement("tr");
+  const totalCell = document.createElement("td");
+  totalCell.setAttribute("colspan", "2");
+  totalCell.textContent = `Total Price: Rs ${total}`;
+  totalRow.appendChild(totalCell);
+
+  // Insert the total row at the end of the table
+  const table = document.querySelector("table");
+  table.appendChild(totalRow);
   
 };
 
